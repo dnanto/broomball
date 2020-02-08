94138fefa7ec5948c24638499596831d25c9c878
dnanto <dnegron2@gmu.edu>
2020-02-08 11:19:28 -0500

    forgot to include in last commit...

--- stats.R
+++ stats.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
+iso_season <- c(`21` = "Spring", `22` = "Summer", `23` = "Autumn", `24` = "Winter")
+
 datatableize <- function(df)
 {
   DT::datatable(
@@ -19,6 +21,7 @@ overall.2 <- function(df.point, df.penalty)
     EVG = integer(), EVA = integer(), EVA1 = integer(), EVA2 = integer(), 
     PPG = integer(), PPA = integer(), PPA1 = integer(), PPA2 = integer(), 
     SHG = integer(), SHA = integer(), SHA1 = integer(), SHA2 = integer(), 
+    ENG = integer(), ENA = integer(), ENA1 = integer(), ENA2 = integer(), 
     PEN = integer(), PIM = integer(),
     stringsAsFactors = F
   )
@@ -32,6 +35,9 @@ overall.2 <- function(df.point, df.penalty)
     rename(mutate(count(filter(df.point, SH == 1), shooter), type = "SHG"), player = shooter),
     rename(mutate(count(filter(df.point, SH == 1), assist1), type = "SHA1"), player = assist1),
     rename(mutate(count(filter(df.point, SH == 1), assist2), type = "SHA2"), player = assist2),
+    rename(mutate(count(filter(df.point, EN == 1), shooter), type = "ENG"), player = shooter),
+    rename(mutate(count(filter(df.point, EN == 1), assist1), type = "ENA1"), player = assist1),
+    rename(mutate(count(filter(df.point, EN == 1), assist2), type = "ENA2"), player = assist2),
     mutate(summarise(group_by(df.penalty, player), n = length(duration)), type = "PEN"),
     mutate(summarise(group_by(df.penalty, player), n = sum(duration)), type = "PIM")
   ) %>% 
@@ -43,7 +49,7 @@ overall.2 <- function(df.point, df.penalty)
     P = EVG + EVA1 + EVA2 + PPG + PPA1 + PPA2 + SHG + SHA1 + SHA2,
     G = EVG + PPG + SHG,
     A = EVA1 + EVA2 + PPA1 + PPA2 + SHA1 + SHA2,
-    EVA = EVA1 + EVA2, PPA = PPA1 + PPA2, SHA = SHA1 + SHA2
+    EVA = EVA1 + EVA2, PPA = PPA1 + PPA2, SHA = SHA1 + SHA2, ENA = ENA1 + ENA2
   ) %>%
   arrange(
     desc(P), 
@@ -52,5 +58,5 @@ overall.2 <- function(df.point, df.penalty)
     desc(PPA2), desc(EVA2), desc(SHA2), 
     PEN, PIM
   ) %>%
-  select(player, P, G, A, EVG, EVA, EVA1, EVA2, PPG, PPA, PPA1, PPA2, SHG, SHA, SHA1, SHA2, PEN, PIM)
+  select(player, P, G, A, EVG, EVA, EVA1, EVA2, PPG, PPA, PPA1, PPA2, SHG, SHA, SHA1, SHA2, ENG, ENA, ENA1, ENA2, PEN, PIM)
 }
